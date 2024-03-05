61ab535a40e72b025c813b28175457701050e165
Mel Lee <hmleeaf@gmail.com>
2024-03-04 23:19:39 -0800

    spawn grounds, add chaser mechanic, change bullet bounds back to script based

--- Assets/Scripts/BounceOnBounds.cs
+++ Assets/Scripts/BounceOnBounds.cs
@@ -2,61 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BounceOnBounds : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class BounceOnXBounds : MonoBehaviour
 {
-    public enum Axis
+    [SerializeField] float boundsMin = -10f;
+    [SerializeField] float boundsMax = 10f;
+
+    Rigidbody rb;
+    private void Start()
     {
-        X, Y, Z
+        rb = GetComponent<Rigidbody>();
     }
-    [SerializeField] Axis boundAxis;
-    [SerializeField] float boundsMin = 10f;
-    [SerializeField] float boundsMax = -10f;
 
     private void FixedUpdate()
     {
         if (transform.position.x < boundsMin)
         {
-            Vector3 forward = transform.forward;
             Vector3 position = transform.position;
-            switch (boundAxis)
-            {
-                case Axis.X:
-                    position.x = boundsMin;
-                    forward.x = -forward.x;
-                    break;
-                case Axis.Y:
-                    position.y = boundsMin;
-                    forward.y = -forward.y;
-                    break;
-                case Axis.Z:
-                    position.z = boundsMin;
-                    forward.z = -forward.z;
-                    break;
-            }
-            transform.forward = forward;
+            position.x = boundsMin;
             transform.position = position;
+
+            rb.velocity = Vector3.Reflect(rb.velocity, Vector3.right);
         }
         else if (transform.position.x > boundsMax)
         {
-            Vector3 forward = transform.forward;
             Vector3 position = transform.position;
-            switch (boundAxis)
-            {
-                case Axis.X:
-                    position.x = boundsMax;
-                    forward.x = -forward.x;
-                    break;
-                case Axis.Y:
-                    position.y = boundsMax;
-                    forward.y = -forward.y;
-                    break;
-                case Axis.Z:
-                    position.z = boundsMax;
-                    forward.z = -forward.z;
-                    break;
-            }
-            transform.forward = forward;
+            position.x = boundsMax;
             transform.position = position;
+
+            rb.velocity = Vector3.Reflect(rb.velocity, Vector3.right);
         }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,20 +4,103 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Player")]
     [SerializeField] PlayerController playerController;
 
-    private float progress = 0;
+    [Header("Smoothing")]
+    [SerializeField, Range(0f, 1f)] float smoothingFactor = 0.5f;
+
+    [Header("Pre-generation")]
+    [SerializeField] float pregenDistance = 30f;
+
+    [Header("Waves")]
+    [SerializeField] List<GameObject> waves;
+    [SerializeField] float wavesGap = 10f;
+    [SerializeField] Transform wavesContainer;
+
+    [Header("Ground")]
+    [SerializeField] GameObject groundPrefab;
+    [SerializeField] float groundLength;
+    [SerializeField] Transform groundsContainer;
+
+    [Header("Chaser")]
+    [SerializeField] GameObject chaser;
+    [SerializeField] float chaserNaturalProgressionRate = 5f;
+
+    float progress = 0;
+    float chaserProgress = 0;
     Vector3 initialCameraPosition;
+    Vector3 initialChaserPosition;
+    int wavesSpawned = 0;
+    int groundsSpawned = 0;
 
     private void Start()
     {
         initialCameraPosition = Camera.main.transform.position;
+        initialChaserPosition = chaser.transform.position;
     }
 
     private void Update()
     {
+        CalcProgress();
+
+        UpdateCamZ();
+        GenerateWave();
+        GenerateGround();
+        UpdateChaser();
+    }
+
+    void CalcProgress()
+    {
         progress = Mathf.Max(progress, playerController.transform.position.z);
+    }
+
+    void UpdateCamZ()
+    {
         Vector3 newCamPos = initialCameraPosition + Vector3.forward * progress;
-        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newCamPos, 1 / Vector3.Distance(Camera.main.transform.position, newCamPos));
+        float distance = Vector3.Distance(Camera.main.transform.position, newCamPos);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newCamPos, 1/distance);
+    }
+
+    void GenerateWave()
+    {
+        while (wavesSpawned * wavesGap < progress + pregenDistance)
+        {
+            SpawnPrefab(waves[Random.Range(0, waves.Count)], ++wavesSpawned * wavesGap, wavesContainer);
+        }
+    }
+
+    void GenerateGround()
+    {
+        while (groundsSpawned * groundLength < progress + pregenDistance)
+        {
+            SpawnPrefab(groundPrefab, ++groundsSpawned * groundLength, groundsContainer);
+        }
+    }
+
+    void SpawnPrefab(GameObject prefab, float zOffset, Transform parent)
+    {
+        GameObject obj = Instantiate(prefab);
+        obj.transform.position += Vector3.forward * zOffset;
+        if (parent != null)
+        {
+            obj.transform.SetParent(parent);
+        }
+    }
+
+    void UpdateChaser()
+    {
+        Vector3 newChaserPos = initialChaserPosition + Vector3.forward * progress;
+        chaser.transform.position += Vector3.forward * chaserNaturalProgressionRate * Time.deltaTime;
+        if (chaser.transform.position.z < newChaserPos.z)
+        {
+            chaser.transform.position = newChaserPos;
+        }
+
+    }
+
+    public void TransitionToAbberation()
+    {
+        Debug.Log("Transitioning");
     }
 }
